9e7ecfbe1f7b0661d3f00eb04520673eb960cec9
Benjamin Hamrick <benthesim@gmail.com>
2017-01-21 15:33:47 -0500

    Made cooldown on gun global.

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -11,44 +11,6 @@ public class Gun : MonoBehaviour
 		#region Attributes
 		public GameObject wave;// Holds the actual wave gameobject that will be spawned
 		public Wave.WaveType wType;// holds what kind of wave the ammo is
-		public int maxAmmoCount;// holds the maximum amount of ammo you can have for this wave
-		public int currentAmmoCount;// holds how much ammo is currently left of this wave
-		public bool alreadyRefilling;// holds whether there is already a coroutine that is refilling the ammo of this wave
-		private bool isOutOfAmmo;// Holds whether there is any ammo left or not
-		#endregion
-
-		#region Proporties
-		public bool IsOutOfAmmo
-		{
-			get{
-				return isOutOfAmmo;
-			}
-		}
-
-		public int MaxAmmoCount
-		{
-			get{
-				return maxAmmoCount;
-			}
-		}
-
-		public int CurrentAmmoCount
-		{
-			get{
-				return currentAmmoCount;
-			}
-			set{
-				// Go on cooldown if the ammo count reachs or goes below 0
-				currentAmmoCount = value;
-				isOutOfAmmo = (currentAmmoCount <= 0) ? true : false;
-				if(isOutOfAmmo){
-					currentAmmoCount = 0;
-				}
-				else if(currentAmmoCount > MaxAmmoCount){
-					currentAmmoCount = MaxAmmoCount;
-				}
-			}
-		}
 		#endregion
 	
 		/// <summary>
@@ -57,21 +19,9 @@ public class Gun : MonoBehaviour
 		/// <param name="gun">Gun.</param>
 		public void SpawnWave(Gun gun, Team team)
 		{
-			if(!IsOutOfAmmo)// Make sure we aren't on cooldown
-			{
-				CurrentAmmoCount--;
-				Vector3 positionAdjuster = (team == Team.blue) ? gun.waveSpawnPostionAdjuster : gun.waveSpawnPostionAdjuster * -1;
-				GameObject w = GameObject.Instantiate(wave, gun.gameObject.transform.position + gun.gameObject.transform.forward + positionAdjuster , Quaternion.identity);
-				w.GetComponent<Wave>().SetTeam(team);
-			}
-		}
-
-		/// <summary>
-		/// Should refill the ammo count to max
-		/// </summary>
-		public void RefillAmmo()
-		{
-			CurrentAmmoCount++;
+			Vector3 positionAdjuster = (team == Team.blue) ? gun.waveSpawnPostionAdjuster : gun.waveSpawnPostionAdjuster * -1;
+			GameObject w = GameObject.Instantiate(wave, gun.gameObject.transform.position + gun.gameObject.transform.forward + positionAdjuster , Quaternion.identity);
+			w.GetComponent<Wave>().SetTeam(team);
 		}
 	}
 		
@@ -79,14 +29,28 @@ public class Gun : MonoBehaviour
 	public WaveAmmo currentWave;// Holds the current wave teh gun can shot
 	public bool isFiring;// holds whether the player is trying to fire the gun or not
 	public float fireRate;// Holds how fast the gun can shot waves
-	public float ammoRefillRate;// holds the amount of time in seconds it takes for one 1
+    public int maxAmmoCount;// holds the maximum amount of ammo you can have for this wave
+    public int currentAmmoCount;// holds how much ammo is currently left of this wave
+    public bool alreadyRefilling;// holds whether there is already a coroutine that is refilling the ammo of this wave
+    private bool isOutOfAmmo;// Holds whether there is any ammo left or not
+    public float ammoRefillRate;// holds the amount of time in seconds it takes for one 1
 	public Vector3 waveSpawnPostionAdjuster;// This adjust the position that the waves spawn at
 	private Team team; // holds which team the player using this gun is from
-	#endregion
+    #endregion
 
-	#region Initializing
-	void Start()
+    #region Properties
+    public bool IsOutOfAmmo 
+    {
+        get {
+            return isOutOfAmmo = currentAmmoCount <= 0;
+        }
+    }
+    #endregion
+
+    #region Initializing
+    void Start()
 	{
+        currentAmmoCount = maxAmmoCount;
 		team = GetComponent<PlayerController>().team;
 	}
 	#endregion
@@ -120,28 +84,37 @@ public class Gun : MonoBehaviour
 	IEnumerator FireWave(WaveAmmo wAmmo)
 	{
 		isFiring = true;
-		while(!wAmmo.IsOutOfAmmo && isFiring)
-		{
-			wAmmo.SpawnWave(this, team);
-			if(wAmmo.CurrentAmmoCount != wAmmo.MaxAmmoCount && !wAmmo.alreadyRefilling){
-				StartCoroutine(RefillingAmmo(wAmmo));
-			}
-			yield return new WaitForSeconds(fireRate);
-		}
-		isFiring = false;
+        if (!IsOutOfAmmo) {
+            wAmmo.SpawnWave(this, team);
+            currentAmmoCount--;
+            if (currentAmmoCount != maxAmmoCount && !alreadyRefilling) {
+                StartCoroutine(RefillingAmmo());
+            }
+
+            yield return new WaitForSeconds(fireRate);
+        }
+        isFiring = false;
 	}
 	#endregion
 
 	#region Refilling 
-	IEnumerator RefillingAmmo(WaveAmmo wAmmo)
+	IEnumerator RefillingAmmo()
 	{
-		wAmmo.alreadyRefilling = true;
-		while(wAmmo.CurrentAmmoCount != wAmmo.MaxAmmoCount)
+		alreadyRefilling = true;
+		while(currentAmmoCount != maxAmmoCount)
 		{
 			yield return new WaitForSeconds(ammoRefillRate);
-			wAmmo.RefillAmmo();
+			RefillAmmo();
 		}
-		wAmmo.alreadyRefilling = false;
+		alreadyRefilling = false;
 	}
-	#endregion
+
+    /// <summary>
+    /// Should refill the ammo count to max
+    /// </summary>
+    public void RefillAmmo()
+    {
+        currentAmmoCount++;
+    }
+    #endregion
 }
