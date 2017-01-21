86401b4a4d22299f38c56e4f02684bd23750d080
Benjamin Hamrick <benthesim@gmail.com>
2017-01-20 21:51:09 -0500

    Added scifi and cartoon effects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,78 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public float gravityScale = 1f;
+    public float ghostJumpTime = 1f;
+    public float movementForce = 50f;
+    public float maxVelocity = 10f;
+    public float sizeScale = 2f;
+    public float jumpForce = 1300f;
+
+    Rigidbody2D rigidBody2D;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        CheckGound();
+    }
+
+    //void CheckGound()
+    //{
+    //    Vector2 direction = Vector2.down;
+    //    Vector2 left = Vector3.Cross(direction, Vector3.forward);
+    //    left.Normalize();
+    //    left *= .4f;
+    //    direction.Normalize();
+    //    float distance = .8f * transform.localScale.x;
+
+    //    RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position, direction, distance);
+    //    if (hit.collider != null && (hit.collider.gameObject.tag == "Floor" ||
+    //        (hit.collider.gameObject.tag == "Ball" && hit.collider.gameObject != gameObject))) {
+    //        Debug.DrawLine((Vector2)transform.position, (Vector2)transform.position + direction * distance, Color.green);
+    //        if (!isOnGround) {
+    //            PerlinShake.instance.PlayShake(0.2f, 10.0f, 0.1f);
+    //            PlayBounceSound();
+    //            isOnGround = true;
+    //            ghostJumpTimer = 0f;
+    //        }
+    //        return;
+    //    }
+    //    Debug.DrawLine((Vector2)transform.position, (Vector2)transform.position + direction * distance, Color.red);
+
+    //    hit = Physics2D.Raycast((Vector2)transform.position + left, direction, distance);
+    //    if (hit.collider != null && (hit.collider.gameObject.tag == "Floor" ||
+    //        (hit.collider.gameObject.tag == "Ball" && hit.collider.gameObject != gameObject))) {
+    //        Debug.DrawLine((Vector2)transform.position + left, (Vector2)transform.position + left + direction * distance, Color.green);
+    //        if (!isOnGround) {
+    //            PerlinShake.instance.PlayShake(0.2f, 10.0f, 0.1f);
+    //            PlayBounceSound();
+    //            isOnGround = true;
+    //            ghostJumpTimer = 0f;
+    //        }
+    //        return;
+    //    }
+    //    Debug.DrawLine((Vector2)transform.position + left, (Vector2)transform.position + left + direction * distance, Color.red);
+
+    //    hit = Physics2D.Raycast((Vector2)transform.position - left, direction, distance);
+    //    if (hit.collider != null && (hit.collider.gameObject.tag == "Floor" ||
+    //        (hit.collider.gameObject.tag == "Ball" && hit.collider.gameObject != gameObject))) {
+    //        Debug.DrawLine((Vector2)transform.position - left, (Vector2)transform.position - left + direction * distance, Color.green);
+    //        if (!isOnGround) {
+    //            PerlinShake.instance.PlayShake(0.2f, 10.0f, 0.1f);
+    //            PlayBounceSound();
+    //            isOnGround = true;
+    //            ghostJumpTimer = 0f;
+    //        }
+    //        return;
+    //    }
+    //    Debug.DrawLine((Vector2)transform.position - left, (Vector2)transform.position - left + direction * distance, Color.red);
+    //    ghostJumpTimer += Time.deltaTime;
+    //    if (ghostJumpTimer > ghostJumpTime) {
+    //        isOnGround = false;
+    //    }
+    //}
 }
