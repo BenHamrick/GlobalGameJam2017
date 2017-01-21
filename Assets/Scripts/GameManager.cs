b6a03ecb919561ce944ed5374d882537d3f4a596
Benjamin Hamrick <benthesim@gmail.com>
2017-01-21 16:49:19 -0500

    no message

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -53,7 +53,7 @@ public class GameManager : MonoBehaviour {
     public void MyCallbackEventHandler(BeatDetection.EventInfo eventInfo) {
 
         Debug.Log(eventInfo.messageInfo);
-
+        Camera.main.backgroundColor = Random.ColorHSV();
     }
 
 
