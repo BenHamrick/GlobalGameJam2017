7fbae8bb841e23eae50478d47da4cc69bd35ebde
Benjamin Hamrick <benthesim@gmail.com>
2017-01-20 21:56:24 -0500

    setup incontrol, added perlin shake

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,9 @@ public class PlayerController : MonoBehaviour {
     public float sizeScale = 2f;
     public float jumpForce = 1300f;
 
+    bool isOnGround = true;
+    float ghostJumpTimer = 0f;
+
     Rigidbody2D rigidBody2D;
 
 	// Use this for initialization
@@ -23,59 +26,56 @@ public class PlayerController : MonoBehaviour {
         CheckGound();
     }
 
-    //void CheckGound()
-    //{
-    //    Vector2 direction = Vector2.down;
-    //    Vector2 left = Vector3.Cross(direction, Vector3.forward);
-    //    left.Normalize();
-    //    left *= .4f;
-    //    direction.Normalize();
-    //    float distance = .8f * transform.localScale.x;
+    void CheckGound()
+    {
+        Vector2 direction = Vector2.down;
+        Vector2 left = Vector3.Cross(direction, Vector3.forward);
+        left.Normalize();
+        left *= .4f;
+        direction.Normalize();
+        float distance = .8f * transform.localScale.x;
 
-    //    RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position, direction, distance);
-    //    if (hit.collider != null && (hit.collider.gameObject.tag == "Floor" ||
-    //        (hit.collider.gameObject.tag == "Ball" && hit.collider.gameObject != gameObject))) {
-    //        Debug.DrawLine((Vector2)transform.position, (Vector2)transform.position + direction * distance, Color.green);
-    //        if (!isOnGround) {
-    //            PerlinShake.instance.PlayShake(0.2f, 10.0f, 0.1f);
-    //            PlayBounceSound();
-    //            isOnGround = true;
-    //            ghostJumpTimer = 0f;
-    //        }
-    //        return;
-    //    }
-    //    Debug.DrawLine((Vector2)transform.position, (Vector2)transform.position + direction * distance, Color.red);
+        RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position, direction, distance);
+        if (hit.collider != null && (hit.collider.gameObject.tag == "Floor" ||
+            (hit.collider.gameObject.tag == "Ball" && hit.collider.gameObject != gameObject))) {
+            Debug.DrawLine((Vector2)transform.position, (Vector2)transform.position + direction * distance, Color.green);
+            if (!isOnGround) {
+                PerlinShake.instance.PlayShake(0.2f, 10.0f, 0.1f);
+                isOnGround = true;
+                ghostJumpTimer = 0f;
+            }
+            return;
+        }
+        Debug.DrawLine((Vector2)transform.position, (Vector2)transform.position + direction * distance, Color.red);
 
-    //    hit = Physics2D.Raycast((Vector2)transform.position + left, direction, distance);
-    //    if (hit.collider != null && (hit.collider.gameObject.tag == "Floor" ||
-    //        (hit.collider.gameObject.tag == "Ball" && hit.collider.gameObject != gameObject))) {
-    //        Debug.DrawLine((Vector2)transform.position + left, (Vector2)transform.position + left + direction * distance, Color.green);
-    //        if (!isOnGround) {
-    //            PerlinShake.instance.PlayShake(0.2f, 10.0f, 0.1f);
-    //            PlayBounceSound();
-    //            isOnGround = true;
-    //            ghostJumpTimer = 0f;
-    //        }
-    //        return;
-    //    }
-    //    Debug.DrawLine((Vector2)transform.position + left, (Vector2)transform.position + left + direction * distance, Color.red);
+        hit = Physics2D.Raycast((Vector2)transform.position + left, direction, distance);
+        if (hit.collider != null && (hit.collider.gameObject.tag == "Floor" ||
+            (hit.collider.gameObject.tag == "Ball" && hit.collider.gameObject != gameObject))) {
+            Debug.DrawLine((Vector2)transform.position + left, (Vector2)transform.position + left + direction * distance, Color.green);
+            if (!isOnGround) {
+                PerlinShake.instance.PlayShake(0.2f, 10.0f, 0.1f);
+                isOnGround = true;
+                ghostJumpTimer = 0f;
+            }
+            return;
+        }
+        Debug.DrawLine((Vector2)transform.position + left, (Vector2)transform.position + left + direction * distance, Color.red);
 
-    //    hit = Physics2D.Raycast((Vector2)transform.position - left, direction, distance);
-    //    if (hit.collider != null && (hit.collider.gameObject.tag == "Floor" ||
-    //        (hit.collider.gameObject.tag == "Ball" && hit.collider.gameObject != gameObject))) {
-    //        Debug.DrawLine((Vector2)transform.position - left, (Vector2)transform.position - left + direction * distance, Color.green);
-    //        if (!isOnGround) {
-    //            PerlinShake.instance.PlayShake(0.2f, 10.0f, 0.1f);
-    //            PlayBounceSound();
-    //            isOnGround = true;
-    //            ghostJumpTimer = 0f;
-    //        }
-    //        return;
-    //    }
-    //    Debug.DrawLine((Vector2)transform.position - left, (Vector2)transform.position - left + direction * distance, Color.red);
-    //    ghostJumpTimer += Time.deltaTime;
-    //    if (ghostJumpTimer > ghostJumpTime) {
-    //        isOnGround = false;
-    //    }
-    //}
+        hit = Physics2D.Raycast((Vector2)transform.position - left, direction, distance);
+        if (hit.collider != null && (hit.collider.gameObject.tag == "Floor" ||
+            (hit.collider.gameObject.tag == "Ball" && hit.collider.gameObject != gameObject))) {
+            Debug.DrawLine((Vector2)transform.position - left, (Vector2)transform.position - left + direction * distance, Color.green);
+            if (!isOnGround) {
+                PerlinShake.instance.PlayShake(0.2f, 10.0f, 0.1f);
+                isOnGround = true;
+                ghostJumpTimer = 0f;
+            }
+            return;
+        }
+        Debug.DrawLine((Vector2)transform.position - left, (Vector2)transform.position - left + direction * distance, Color.red);
+        ghostJumpTimer += Time.deltaTime;
+        if (ghostJumpTimer > ghostJumpTime) {
+            isOnGround = false;
+        }
+    }
 }
