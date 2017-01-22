4775a742e1ae52e83e3dc3437932d32c8ae5e653
Benjamin Hamrick <benthesim@gmail.com>
2017-01-22 12:17:03 -0500

    Added powerups

---
+++ Assets/Scripts/PowerUpController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpController : MonoBehaviour {
+
+    public GameObject healthPowerup;
+    public GameObject energyPowerup;
+
+    float requieredHealthTime;
+    float requieredEnergyTime;
+
+    float healthTimer;
+    float energyTimer;
+
+	// Use this for initialization
+	void Start () {
+        requieredHealthTime += 30f;
+        requieredEnergyTime += 40f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameManager.instance.gameState != GameState.gamePlay) {
+            return;
+        }
+        if (healthPowerup.activeInHierarchy == false) {
+            healthTimer += Time.deltaTime;
+        }
+        if (energyPowerup.activeInHierarchy == false) {
+            energyTimer += Time.deltaTime;
+        }
+
+        if (healthTimer > requieredHealthTime) {
+            requieredHealthTime += 30f;
+            healthPowerup.SetActive(true);
+        }
+
+        if (energyTimer > requieredEnergyTime) {
+            requieredEnergyTime += 30f;
+            energyPowerup.SetActive(true);
+        }
+    }
+}
