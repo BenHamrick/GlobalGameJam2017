55c7a8515a719ce86f1c94d93a72159aa5be188f
Dorian McDougald <demcdougald@gmail.com>
2017-01-22 14:35:42 -0500

    changed it so you can't be revived after the game has ended

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -47,11 +47,8 @@ public class GameManager : MonoBehaviour {
     public List<MusicTextSync> textSync;
     public List<MusicHueSync> hueSync;
 
-    public int colorIndex;
-
     void Awake()
     {
-        colorIndex = 0;
         textSync = new List<MusicTextSync>();
         hueSync = new List<MusicHueSync>();
         pressStart.SetActive(true);
@@ -220,6 +217,10 @@ public class GameManager : MonoBehaviour {
 				positionFound = true;
 				player.gameObject.transform.position = positions[positionIndex].position;
 			}
+		}
+
+		if(gameState == GameState.endOfGame){
+			return null;
 		}
 
 		player.RevivePlayer();
